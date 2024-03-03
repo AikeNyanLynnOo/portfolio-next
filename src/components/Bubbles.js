0d70b57d684262293a5990df1ed9e77a728ae066
AikeNyanLynnOo <aikenyanlynnoo.dev@gmail.com>
2024-03-03 14:05:25 +0630

    - update bubbles to random

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -1,5 +1,5 @@
 import { clsx } from "clsx";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useMemo } from "react";
 import { Fragment, useEffect } from "react";
 import { useSelector } from "react-redux";
@@ -19,7 +19,7 @@ export const Bubbles = ({
       "dark:from-teal-200": true,
       "to-cyan-500": true,
       "dark:to-cyan-500": true,
-    //   "via-purple-500" : true,
+      //   "via-purple-500" : true,
       ...customBubbleDotClasses,
     });
   }, [customBubbleDotClasses]);
@@ -33,16 +33,46 @@ export const Bubbles = ({
       "dark:from-teal-200": true,
       "to-cyan-500": true,
       "dark:to-cyan-500": true,
-    //   "via-pink-200" : true,
+      //   "via-pink-200" : true,
       ...customBubbleDotClasses,
     });
   }, []);
+
+  const [bubbles, setBubbles] = useState([]);
+
+  const [intervalTime, setIntervalTime] = useState([1000]);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setBubbles(
+        Array.from(
+          { length: Math.floor(Math.random() * 10) + 1 },
+          () => Math.floor(Math.random() * 24), // bubble index
+        ),
+      );
+      setIntervalTime(Math.floor(Math.random() * (10000 - 5000 + 1) + 5000));
+    }, intervalTime);
+
+    // this will clear Timeout
+    // when component unmount like in willComponentUnmount
+    // and show will not change to true
+    return () => {
+      clearInterval(interval);
+    };
+  }, [intervalTime]);
+
   return (
     <div className={`wrapper overflow-hidden ${customClasses}`}>
       {[
-        showIndices &&
-          showIndices.map((index, idx) => (
-            <div key={idx} className={getDotDivClass(index)}>
+        bubbles &&
+          bubbles.map((index, idx) => (
+            <div
+              key={idx}
+              className={getDotDivClass(index)}
+              style={{
+                animationDuration: Math.floor(Math.random() * (15 - 1 + 1) + 1),
+              }}
+            >
               <span className={bubbleDotClasses}></span>
             </div>
           )),
